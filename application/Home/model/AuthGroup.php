57bbf638aaad6ce1eba5a3767f1720aafd2fe57f
xyl <492113976@qq.com>
2016-11-30 16:10:43 +0800

    获取用户组列表、权限列表并入model中

--- application/Home/model/AuthGroup.php
+++ application/Home/model/AuthGroup.php
@@ -13,5 +13,32 @@ use think\Model;
 
 class AuthGroup extends Model
 {
-
+//定义公共获取用户组方法
+    function getGroupList(){
+        $Auth_rule=db('Auth_rule');
+        $grouplist=$this->select();
+        $rulelist=$Auth_rule->select();
+        $i=0;
+        foreach ($grouplist as $group){
+            for($t=0;$t<count($rulelist);$t++){   ///初始化选择项
+                $obr[$t]=0;
+            }
+            $arrrule=explode(',', $group['rules']);
+            foreach ($arrrule as $v=>$u){
+                foreach ($rulelist as $a=>$b){
+                    if($u==$b['id']){
+                        $obr[$a]=1;
+                        break;
+                    }
+                }
+            }
+            $group['rules']=$obr;
+            unset($grouplist[$i]); //删掉原有的键值
+            $grouplist[$i] = $group; //赋值
+            $i++;
+        }
+        $arr['grouplist']=$grouplist;
+        $arr['rulelist']=$rulelist;
+        return $arr;
+    }
 }
--- application/Home/model/User.php
+++ application/Home/model/User.php
@@ -13,5 +13,17 @@ use think\Model;
 
 class User extends Model
 {
-
+    //定义公共获取用户方法
+    function getUserList(){
+        $Auth_group=db('Auth_group');
+        $join = [
+            ['tp_auth_group_access w','a.admin_id=w.uid'],
+            ['tp_auth_group c','w.group_id=c.id'],
+        ];
+        $userlist=$this->alias('a')->join($join)->select();
+        $grouplist=$Auth_group->select();
+        $arr['userlist']=$userlist;
+        $arr['grouplist']=$grouplist;
+        return $arr;
+    }
 }
