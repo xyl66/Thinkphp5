24a098e28795b276de6765e0c8824c28d27abfa8
xyl <492113976@qq.com>
2016-12-16 17:43:50 +0800

    IE版本提示

---
+++ application/push/controller/Worker.php
@@ -0,0 +1,64 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: F3233253
+ * Date: 2016/12/14
+ * Time: 上午 09:38
+ */
+
+namespace app\push\controller;
+
+use think\worker\Server;
+
+class Worker extends Server
+{
+    protected $socket = 'websocket://10.130.2.95:2346';
+
+    /**
+     * 收到信息
+     * @param $connection
+     * @param $data
+     */
+    public function onMessage($connection, $data)
+    {
+        $connection->send('我收到你的信息了');
+    }
+
+    /**
+     * 当连接建立时触发的回调函数
+     * @param $connection
+     */
+    public function onConnect($connection)
+    {
+
+    }
+
+    /**
+     * 当连接断开时触发的回调函数
+     * @param $connection
+     */
+    public function onClose($connection)
+    {
+
+    }
+
+    /**
+     * 当客户端的连接上发生错误时触发
+     * @param $connection
+     * @param $code
+     * @param $msg
+     */
+    public function onError($connection, $code, $msg)
+    {
+        echo "error $code $msg\n";
+    }
+
+    /**
+     * 每个进程启动
+     * @param $worker
+     */
+    public function onWorkerStart($worker)
+    {
+
+    }
+}
---
+++ server.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: F3233253
+ * Date: 2016/12/14
+ * Time: 上午 09:35
+ */
+#!/usr/bin/env php
+define('APP_PATH', __DIR__ . '/application/');
+define('BIND_MODULE','push/Worker');
+// 加载框架引导文件
+require __DIR__ . '/thinkphp/start.php';
